5612b7747fd8505a706c4b2906cd656f13c16232
Nicholas Heneisen <nick@heneisen.com>
2017-06-29 11:43:16 -0400

    Drawer functioning properly. Added empty model and controller documents for saving searches.

--- app/components/Main.js
+++ app/components/Main.js
@@ -13,26 +13,48 @@ import RaisedButton from 'material-ui/RaisedButton';
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {open: false};
+    this.state = {
+      open: false,
+      currentSearch: "Homo sapiens"
+    };
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleClose = this.handleClose.bind(this);
   }
-  
+
+
+
+  searchSet(newSearch) {
+    this.setState({
+      currentSearch: newSearch
+    })
+  }
+
   handleToggle () {
-     this.setState({open: !this.state.open});
+    this.setState({open: !this.state.open});
+    console.log("Toggled. Is drawer open? " + this.state.open);
   }
+
+  
+  handleClose () {
+    this.setState({open: false})
+    console.log("Closed. Is drawer open? " + this.state.open);
+  };
+
+
 
   render() {
     return (
       <div id="main">
-        <SpeciesSearch />
+        <SpeciesSearch searchSet={this.searchSet} />
         <Login />
+        
+        <h1
+          label="Toggle Drawer"
+          onClick={this.handleToggle}>
+          {this.state.currentSearch}
+        </h1>
         <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-          <RaisedButton
-            label="Toggle Drawer"
-            onClick={this.handleToggle}
-          />
-        </MuiThemeProvider>
-        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-        <Drawer open={this.state.open}>
+        <Drawer open={this.state.open} width={325}>
           <MenuItem>Menu Item</MenuItem>
           <MenuItem>Menu Item 2</MenuItem>
         </Drawer>
